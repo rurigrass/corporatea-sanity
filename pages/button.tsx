fb604580aeaeb8a35fadc71f29da4e69c156c77c
Ruairidh Grass <ruripg@gmail.com>
2023-02-08 23:46:53 +0100

    most of button component working

--- pages/button.tsx
+++ pages/button.tsx
@@ -9,17 +9,19 @@ interface IFormInput {
 }
 
 const Button = () => {
-    const [searchInput, setSearchInput] = useState<string>("")
     const [selectedCompany, setCompany] = useState<ICompany>({
         _id: "0",
         image: null,
-        name: "no name",
+        name: "poopp name",
         slug: null
     })
 
+    // console.log("SELECTED COMPANY ", selectedCompany);
+
+
     const companies = [
         { _id: "0", image: null, name: "no name", slug: null },
-        { _id: "1", image: null, name: "no name", slug: null }
+        { _id: "1", image: null, name: "poo name", slug: null }
     ]
 
     const {
@@ -35,28 +37,27 @@ const Button = () => {
 
     return (
         <div>
-            {companies.map((company) =>
-                <li key={company._id} className={`first:rounded-t-xl last:rounded-b-xl bg-gray-light text-blue-shady hover:bg-blue-tintish hover:text-white ${company.name.toLowerCase().startsWith(searchInput) ? "block" : "hidden"}`}>
-                    <button
-                        type="button"
-                        onClick={(e) => {
-                            e.preventDefault(), setCompany(company);
-                        }}
-                        className="flex items-center space-x-2 px-3 py-2 w-full" title={company.name} {...register("company")}>
-                        {company.image ? (
-                            <img
-                                className="h-10 w-10 rounded-full mr-2"
-                                src={urlFor(company.image).url()!}
-                                alt="" />
-                        ) : <div className="h-10 w-10 rounded-full bg-pink-tintier mr-2" />}
-                        {truncate(company.name, 15)}</button>
-                </li>
-            )}
-            <input
-                type="button"
-                className="h-5 w-10 bg-green-shady"
-                value="Add to favorites"
-            />
+            <form onSubmit={handleSubmit(onSubmit)}>
+                {companies.map((company) =>
+                    <li key={company._id} className={`first:rounded-t-xl last:rounded-b-xl bg-gray-light text-blue-shady hover:bg-blue-tintish hover:text-white ${company.name.toLowerCase() ? "block" : "hidden"}`}>
+                        <input
+                            type="button"
+                            title={company.name}
+                            {...register("company")}
+                            onClick={(e) => {
+                                e.preventDefault(), setCompany(company)
+                            }}
+                            className="flex items-center space-x-2 px-3 py-2 w-full hover:cursor-pointer"
+                            value={selectedCompany.name} />
+                        {company.name}
+                    </li>
+                )}
+                <input
+                    type="submit"
+                    className="h-5 w-10 bg-green-shady"
+                    value="Add to favorites"
+                />
+            </form>
         </div>
     )
 }
